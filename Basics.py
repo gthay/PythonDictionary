58c4dcc8a910b33512d41a9516f9bf6f2e2e403e
gthay <52482195+gthay@users.noreply.github.com>
2020-09-23 11:39:51 +0800

    creation

---
+++ Basics.py
@@ -0,0 +1,30 @@
+
+# ' is the same as "
+
+#print
+print("Hello, World!")
+
+#var
+x = 5
+
+int_ = 5
+float_ = 5.5
+list_ = ["one", "two", "three"]
+dict_ = {"name":"john", "age":36}
+
+x = int(5.5)
+x = str(5.5)
+x = float(5.5)
+
+
+#print var
+print("x is: "+ x)
+
+#if
+if 5>2:
+    print("if")
+elif 5<2:
+    print("elif")
+else:
+    print("else")
+
